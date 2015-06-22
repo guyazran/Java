4f2c8fef90357f1bc9fcd386837100c5ba5fdacd
Guy Azran <guyazran@Guys-MacBook-Pro.local>
2015-06-22 14:17:11 +0300

    add Fraction to Numbers Array

--- 22_06_15/Inheritance/src/com/company/Main.java
+++ 22_06_15/Inheritance/src/com/company/Main.java
@@ -5,16 +5,13 @@ public class Main {
     public static void main(String[] args) {
         // Number n = new Integer(8); // Integer extends Number. Number cannot directly be a Number type object
 
-        Number[] nums = new Number[4];
+        Number[] nums = new Number[5];
         nums[0] = new Byte((byte)10); // casting is used to enter a byte to Byte's consturctor
         nums[1] = new Integer(10);
         nums[2] = new Float(10.0f);
         nums[3] = new Double(10.0);
+        nums[4] = new Fraction(1, 2);
         System.out.println(sum(nums));
-
-        Fraction f = new Fraction(-2, 10);
-        f.factor();
-        System.out.println(f);
 
     }
 
