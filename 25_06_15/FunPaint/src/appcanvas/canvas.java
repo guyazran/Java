b92624c5c61954d08ca9ae66a63b06ef80c60ad3
Guy Azran <guyazran@Guys-MacBook-Pro.local>
2015-06-25 10:22:27 +0300

    ignore

--- 25_06_15/FunPaint/src/appcanvas/canvas.java
+++ 25_06_15/FunPaint/src/appcanvas/canvas.java
@@ -27,17 +27,6 @@ public class canvas {
     public static double sumOfArea(Shape[] shapes){
         double sum = 0;
 
-//        for (Shape s : shapes){
-//            if (s instanceof Rectangle){
-//                Rectangle rect = (Rectangle)s;
-//                sum += rect.area();
-//            }
-//            if (s instanceof Circle){
-//                Circle c = (Circle)s;
-//                sum = c.area();
-//            }
-//        }
-
         for (Shape s : shapes){
             sum += s.area();
         }
