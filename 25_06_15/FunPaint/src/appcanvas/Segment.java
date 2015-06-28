770667617e6700208eb9ed6c4664f7e371dcc4fc
Guy Azran <guyazran@Guys-MBP.home>
2015-06-28 12:19:15 +0300

    Segment Homework

--- 25_06_15/FunPaint/src/appcanvas/Segment.java
+++ 25_06_15/FunPaint/src/appcanvas/Segment.java
@@ -7,14 +7,99 @@ public class Segment {
     private Point p1;
     private Point p2;
 
+    public Segment(Point p1, Point p2) {
+        setP1(p1);
+        setP2(p2);
+    }
+
+    public Segment(Segment segment){
+        this(segment.getP1(), segment.getP2());
+    }
+
+    public Point getP1() {
+        return new Point(p1);
+    }
+
+    public void setP1(Point p1) {
+        this.p1 = new Point(p1);
+    }
+
+    public Point getP2() {
+        return new Point(p2);
+    }
+
+    public void setP2(Point p2) {
+        this.p2 = new Point(p2);
+    }
+
+    public double length(){
+        return p1.distanceFromPoint(p2);
+    }
+
+    public double slope(){
+        double deltaX = p1.getXpos() - p2.getXpos();
+        double deltaY = p1.getYpos() - p2.getYpos();
+        return (double)deltaY/deltaX;
+    }
+
+    public double distanceFromStraight(Point p){
+//        return Math.abs((p2.getYpos() - p1.getYpos())*p.getXpos() - (p2.getXpos() - p1.getXpos())*p.getYpos()+ p2.getXpos()*p1.getYpos() - p2.getYpos()*p1.getXpos())
+//               /this.length();
+        double otherSlope = this.perpendicularSlope();
+        double otherYintercept = yIntercept(p, otherSlope);
+        double commonX = findCommonX(otherSlope, otherYintercept);
+        double commonY = findCommonY(commonX);
+
+        return distanceFromPoint(p, commonX, commonY);
+    }
+
+    double perpendicularSlope(){
+        return (-1/this.slope());
+    }
+
+    double yIntercept(){
+        return yIntercept(p1, this.slope());
+    }
+
+    double yIntercept(Point p, double slope){
+        return p.getYpos() - (p.getXpos()*slope);
+    }
+
+    double findCommonX(double slope, double yIntercept){
+        return (yIntercept - this.yIntercept())/(this.slope() - slope);
+    }
+
+    double findCommonY(double x){
+        return this.slope()*x + this.yIntercept();
+    }
+
+    static double distanceFromPoint(Point p, double x, double y){
+        double deltaX = x - p.getXpos();
+        double deltaY = y - p.getYpos();
+        return Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+    }
+
+    @Override
+    public String toString() {
+        return "The segment starts at " + p1 + "and ends at" + p2 + ". The linear equation is y = " + this.slope() + "x + " + this.yIntercept();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj == this)
+            return true;
+        if (obj instanceof Segment) {
+            Segment other = new Segment((Segment) obj);
+            return this.p1.equals(other.p1) && this.p2.equals(other.p2);
+        }
+        return false;
+    }
+
     /*
-    line that goes the shortest route between two points
-    no adding fields
-    add constructor, one that takes 2 points and one that takes another Segment and copies it.
-    add setters and getters.
-    prevent aliasing.
     methods:
-    1. returns the length of the segment (doble)
+    1. returns the length of the segment (double)
     2. what is the distance from any point to the straight that continues the segment (double)
     3. return the slope of the line - deltaY/deltaX
 
--- 25_06_15/FunPaint/src/appcanvas/canvas.java
+++ 25_06_15/FunPaint/src/appcanvas/canvas.java
@@ -44,19 +44,27 @@ public class canvas {
 //        System.out.println(c1.equals(c2));
 //        System.out.println(c1.equals(c3));
 
-        //hacking into the xPos or yPos of a circle.
-        Point p1 = new Point(4, 3);
-        Circle c1 = new Circle(p1, 18);
-        p1.setYpos(14); //the cloning constructor activated in getCenter() makes sure that c1.center and p1 point to different objects of the same values
-        System.out.println(c1);
-
-        Point hackedCenter = c1.getCenter(); //after changing getCenter() hackedCenter will Point to a new object
-        hackedCenter.setYpos(100); //this will affect the new  object created in getCenter()
-        System.out.println(c1);
-        /*
-        if we do not wish to allow aliasing or the manipulation of fields that are objects, we must change
-        their getters and setters to be written without aliasing
-         */
+//        //hacking into the xPos or yPos of a circle.
+//        Point p1 = new Point(4, 3);
+//        Circle c1 = new Circle(p1, 18);
+//        p1.setYpos(14); //the cloning constructor activated in getCenter() makes sure that c1.center and p1 point to different objects of the same values
+//        System.out.println(c1);
+//
+//        Point hackedCenter = c1.getCenter(); //after changing getCenter() hackedCenter will Point to a new object
+//        hackedCenter.setYpos(100); //this will affect the new  object created in getCenter()
+//        System.out.println(c1);
+//        /*
+//        if we do not wish to allow aliasing or the manipulation of fields that are objects, we must change
+//        their getters and setters to be written without aliasing
+//         */
+        Point p1 = new Point(0, 2);
+        Point p2 = new Point(6, 0);
+        Segment s = new Segment(p1, p2);
+        System.out.println(s);
+        Point p3 = new Point(-2, -4);
+        Segment perpS = new Segment(p3, p1);
+        System.out.println(perpS);
+        System.out.println(s.distanceFromStraight(p3));
     }
 
     public static double sumOfArea(Shape[] shapes){
