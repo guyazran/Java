95956d1cec0dfacd52cadc14228c4e5af7f6e12a
Guy Azran <guyazran@Guys-MacBook-Pro.local>
2015-06-29 15:56:54 +0300

    Static fields

---
+++ 29_06_15/Static/src/com/company/Dog.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by guyazran on 6/29/15.
+ */
+public class Dog {
+    static int counter = 0;
+    public Dog(){
+        counter++;
+    }
+}
---
+++ 29_06_15/Static/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        Dog d2 = new Dog();
+        System.out.println(Dog.counter);
+    }
+}
