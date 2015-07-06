020106c2f9b944067e5fbfecd85fb29cc759f150
Guy Azran <guyazran@Guys-MacBook-Pro.local>
2015-07-06 15:44:51 +0300

    MyString method signatures

---
+++ 6_7_15/Collections/src/com/company/MyString.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/**
+ * Created by guyazran on 7/6/15.
+ */
+public class MyString {
+    private char[] chars;
+
+    public MyString(char[] chars){
+
+    }
+
+    //returns the beginning of the beginning of the given string
+    public int indexOf(MyString another){
+        return -1;
+    }
+
+    //chars = "hello world today", parameter: ' ', return: {"hello", "world", "today"}
+    public MyString[] split(char delimiter) {
+        return null;
+    }
+
+    public MyString[] split(MyString delimiter) {
+        return null;
+    }
+}
